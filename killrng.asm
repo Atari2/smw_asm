d205643b7248026ac2b4035e9bc8661837cbf0b7
Alessio <53220763+Atari2@users.noreply.github.com>
2020-04-13 16:41:26 +0200

    updated

--- killrng.asm
+++ killrng.asm
@@ -1,6 +1,6 @@
 ;this simple patch basically destroys the rng routine in smw
-;replacing with a very arbitrary routine that always returns $7F
-
+;replacing with a very arbitrary routine that always returns $7F (althought you can change it)
+;note: the rest of the routine isn't NOPed out because I was lazy
 org $01ACF9
 	LDA #$7F		;arbitrary value
 	STA $148C
