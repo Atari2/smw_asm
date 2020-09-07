870bc375f600ccb0ce65b4c9b1945c45ef05856b
Atari2 <alex.robassomero@gmail.com>
2020-09-07 19:41:59 +0200

    Changed invertaccumrec to be more useful and recover 8 bytes from the rom.

--- invertaccumrec.asm
+++ invertaccumrec.asm
@@ -1,5 +1,14 @@
-org $01C45C
-	JSR $804A	; jump to invertaccum
-
 org $01CCEC
 	NOP #4
+
+org $01804A
+	NOP #4
+
+org $01C45C
+	EOR #$FF : INC
+
+org $0197DA
+	EOR #$FF : INC
+
+org $0197E1
+	EOR #$FF : INC
